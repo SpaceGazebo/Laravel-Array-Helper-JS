0a14ff96fd392648e0d67fa5cdd7efda99025906
InstanceOfMichael <michael@sharpcompanies.com>
2014-12-22 14:40:06 -0600

    Update laravel_helpers.js

--- laravel_helpers.js
+++ laravel_helpers.js
@@ -107,3 +107,18 @@ function array_flatten(i)
     }
     return o;
 }
+/**
+ *  replicates array_first feature from laravel
+ */
+ function array_first(a,f,d)
+ {
+    if (!f && a.length)
+    {
+        return a.[0];
+    }
+    for(var x = 0; x < a.length; x++)
+    {
+        if (f(a[x])) return a[x];
+    }
+    return d;
+ }
