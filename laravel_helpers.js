f0ba16b1fe9f3d622d3c81bad45c975e95db8cd5
InstanceOfMichael <michael@sharpcompanies.com>
2015-01-23 12:30:16 -0600

    Added array_fetch, array_has

--- laravel_helpers.js
+++ laravel_helpers.js
@@ -151,3 +151,31 @@ function array_only(a,ks){
   
   return b;
 }
+/**
+ * replicates array_fetch feature from laravel
+ */
+function array_fetch (arr,key)
+{
+    var r = [];
+    for (var k in arr)
+    {
+        if (arr.hasOwnProperty(k))
+        {
+            if (array_has(arr[k],key))
+            {
+                r.push(
+                    array_get(arr[k],key)
+                );
+            }
+        }
+    }
+    return r;
+}
+/**
+ *  This checks if a deeply nested variable is set, similar to Laravel's Input::has('key')
+ */
+function array_has (arr,key)
+{
+    return (array_get(arr,key,false)!==false && array_get(arr,key,true)!==true);
+}
+
