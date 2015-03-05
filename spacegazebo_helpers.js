14806b6363bb793d393f8aaf59f3a34799b8d5ca
InstanceOfMichael <michael@sharpcompanies.com>
2015-03-05 16:04:38 -0600

    Update spacegazebo_helpers.js

--- spacegazebo_helpers.js
+++ spacegazebo_helpers.js
@@ -52,3 +52,84 @@ function assoc_diff (a,b)
     }
     return Δ;
 }
+/**
+ *  returns inputed money value as string representing a valid decimal number
+ *  intended for sending serverside or to be passed to other calls.
+ * 
+ *  OR returns NaN
+ * 
+ *  does not validate arrangements of $,space,comma,period, only strips them
+ */
+function money2DecimalString(m){
+    var v;
+    var p = m;
+    if (typeof p == 'string')
+    {
+        p = money2DecimalArr(m);
+    }
+    if (typeof p == 'number' && isNaN(p))
+    {
+        return p;
+    }
+    if (p.invalid)
+    {
+        return NaN;
+    }
+    
+    p.left = parseInt('0'+p.left,10);
+    
+    if (p.left && p.right)
+    {
+        v = p.left+'.'+p.right;
+    }
+    else if (p.right)
+    {
+        v = '0.'+p.right;
+    }
+    else if (p.left)
+    {
+        v = p.left;
+    }
+    else
+    {
+        v = '0';
+    }
+    if (p.negative && v!=='0')
+    {
+        v = '-'+v;
+    }
+    return v;
+}
+/**
+ *  Helper function for money2DecimalArr
+ * 
+ *  A money format validator may prefer to call this function instead of the above
+ *  if you wanted to check the $,space,comma,period count/order.
+ * 
+ *  returns array describing number
+ */
+function money2DecimalArr(str){
+    if (typeof str == 'number' && isNaN(str)){ return str; }
+    
+    var decimal_pos = Math.max(str.lastIndexOf('.'),str.lastIndexOf(','));
+    
+    if (decimal_pos==-1) decimal_pos = str.length;
+    
+    var num = {
+    left:str.substr(0,decimal_pos).replace(/[^0-9]+/g,""),
+    right:str.substr(decimal_pos+1,str.length).replace(/[^0-9]+/g,""),
+        source:str
+    };
+
+    if (str.trim().indexOf('-')==0)
+    {
+        num.negative = true;
+    }
+
+    if (num.left.length + num.right.length === 0)
+    {
+        num.invalid = true;
+    }
+
+    return num;
+}
